3d970c4dde2c5d9b1ebb8429802d6dfdebd8bce3
Ihor Leonov <ihorleonow@gmail.com>
2024-01-17 12:52:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
     }
 
     return () => {
-      () => dispatch(resetError());
+      dispatch(resetError());
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [error]);
